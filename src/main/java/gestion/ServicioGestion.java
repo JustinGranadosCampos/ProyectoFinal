607809b91fb16e51f6b9b88afdc1e87fdd30e898
Justin Granados <justingranados34@gmail.com>
2020-08-26 11:07:32 -0600

    Crud de servicios listo (funcional)
    Se crea el crud de servicios

--- src/main/java/gestion/ServicioGestion.java
+++ src/main/java/gestion/ServicioGestion.java
@@ -12,10 +12,10 @@ import model.Servicio;
 public class ServicioGestion {
     
     private static final String SQL_SELECT_SERVICIOS="SELECT * FROM SERVICIO";
-    private static final String SQL_SELECT_SERVICIO="SELECT * FROM SERVICIO ID_SERVICIO=?";
+    private static final String SQL_SELECT_SERVICIO="SELECT * FROM SERVICIO WHERE ID_SERVICIO=?";
     private static final String SQL_INSERT_SERVICIO="INSERT INTO SERVICIO(DESC_SERVICIO,CODIGO_SERVICIO,PRECIO) VALUES(?,?,?)";
     private static final String SQL_UPDATE_SERVICIO="UPDATE SERVICIO SET DESC_SERVICIO=?,CODIGO_SERVICIO=?,PRECIO=? WHERE ID_SERVICIO=?";
-    private static final String SQL_DELETE_SERVICIO="UPDATE SERVICIO SET DESC_SERVICIO=?,CODIGO_SERVICIO=?,PRECIO=? WHERE ID_SERVICIO=?";
+    private static final String SQL_DELETE_SERVICIO="DELETE FROM SERVICIO WHERE ID_SERVICIO=?";
     
     public static ArrayList<Servicio> getServicios() {
         ArrayList<Servicio> lista = new ArrayList<>();
@@ -63,7 +63,6 @@ public class ServicioGestion {
             sentencia.setString(1, servicio.getDesc_servicio());
             sentencia.setString(2, servicio.getCodigo_servicio());
             sentencia.setDouble(3, servicio.getPrecio());
-            sentencia.setInt(4, servicio.getId_servicio());
             return sentencia.executeUpdate() > 0;
         } catch (SQLException ex) {
             Logger.getLogger(CitaGestion.class.getName()).log(Level.SEVERE, null, ex);
