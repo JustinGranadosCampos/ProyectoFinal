03d933f7a27640243955b04fecde810cfff12895
daylighted <45243229+daylighted@users.noreply.github.com>
2020-08-27 13:22:54 -0600

    Jasper Report
    Se añade el reporte de factura, ver y descargar PDF

--- src/main/java/controller/ReporteController.java
+++ src/main/java/controller/ReporteController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import java.io.File;
@@ -10,15 +5,12 @@ import java.io.IOException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import model.Conexion;
-import model.Estudiante;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
@@ -32,9 +24,6 @@ import net.sf.jasperreports.engine.JasperPrint;
 @SessionScoped
 public class ReporteController implements Serializable {
 
-    /**
-     * Creates a new instance of ReporteController
-     */
     public ReporteController() {
     }
 
@@ -42,7 +31,7 @@ public class ReporteController implements Serializable {
         try {
             File jasper = new File(FacesContext.
                     getCurrentInstance().
-                    getExternalContext().getRealPath("/prospecto/Prospecto.jasper"));
+                    getExternalContext().getRealPath("/reportes/Fac1.jasper"));
 
             JasperPrint reporteJasper = JasperFillManager
                     .fillReport(jasper.getPath(), null, Conexion.getConexion());
@@ -69,7 +58,7 @@ public class ReporteController implements Serializable {
         try {
             File jasper = new File(FacesContext.
                     getCurrentInstance().
-                    getExternalContext().getRealPath("/prospecto/Prospecto.jasper"));
+                    getExternalContext().getRealPath("/reportes/Fac1.jasper"));
 
             JasperPrint reporteJasper = JasperFillManager
                     .fillReport(jasper.getPath(), null, Conexion.getConexion());
@@ -91,34 +80,4 @@ public class ReporteController implements Serializable {
 
     }
 
-    public void certificacionEstudiante(Estudiante estudiante) {
-        Map<String, Object> parametros = new HashMap<>();
-        parametros.put("idEstudiante", estudiante.getId());
-        parametros.put("nombre", estudiante.getNombreCompleto());
-
-        try {
-            File jasper = new File(FacesContext.
-                    getCurrentInstance().
-                    getExternalContext().getRealPath("/reportes/certificacion.jasper"));
-
-            JasperPrint reporteJasper = JasperFillManager
-                    .fillReport(jasper.getPath(), parametros, Conexion.getConexion());
-
-            HttpServletResponse respuesta
-                    = (HttpServletResponse) FacesContext.getCurrentInstance()
-                            .getExternalContext().getResponse();
-
-            respuesta.setContentType("application/pdf");
-            respuesta.addHeader("Content-Type", "application/pdf");
-
-            ServletOutputStream flujo = respuesta.getOutputStream();
-            JasperExportManager.exportReportToPdfStream(reporteJasper, flujo);
-            FacesContext.getCurrentInstance().responseComplete();
-
-        } catch (JRException | IOException ex) {
-            Logger.getLogger(ReporteController.class.getName()).log(Level.SEVERE, null, ex);
-
-        }
-
-    }
 }
