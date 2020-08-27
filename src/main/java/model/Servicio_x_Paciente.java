959ba6e4e47dc024abc4dfe116277c60f3e3b35d
Justin Granados <justingranados34@gmail.com>
2020-08-26 22:01:51 -0600

    Se inicia el model de servicio x paciente

---
+++ src/main/java/model/Servicio_x_Paciente.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Servicio_x_Paciente {
+    private char recurrente;
+    private int id_pacient;
+    private int id_distrito;
+    private int id_canton;
+    private int id_provincia;
+    private int id_servicio;
+
+    public Servicio_x_Paciente() {
+    }
+    
+    
+}
