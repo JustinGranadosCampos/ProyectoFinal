3715a9d5d797c7c1446732aa3b1f163c2ff9f9ad
Justin Granados <justingranados34@gmail.com>
2020-08-27 00:37:51 -0600

    Frontend de sxp

---
+++ src/main/java/controller/ServicioClienteController.java
@@ -0,0 +1,74 @@
+package controller;
+
+import gestion.ServicioPacienteGestion;
+import java.io.Serializable;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import model.Servicio_x_Paciente;
+
+@Named(value = "servicio_x_ClienteController")
+@RequestScoped
+public class ServicioClienteController extends Servicio_x_Paciente implements Serializable {
+
+    public ServicioClienteController() {
+    }
+    
+    public List<Servicio_x_Paciente> getServiciosPacientes() {
+        return ServicioPacienteGestion.getServiciosPacientes();
+    }
+    
+    public String edita(int id) {/*Boton de editar*/
+        Servicio_x_Paciente sxp = ServicioPacienteGestion.getServicioPaciente(id);
+        if (sxp != null) {
+            this.setId(sxp.getId());
+            this.setRecurrente(sxp.getRecurrente());
+            this.setId_pacient(sxp.getId_pacient());
+            this.setId_distrito(sxp.getId_distrito());
+            this.setId_canton(sxp.getId_canton());
+            this.setId_provincia(sxp.getId_provincia());
+            this.setId_servicio(sxp.getId_servicio());
+            return "editasp.xhtml";
+        } else {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error",
+                    "Posiblemente identificacion ya no exista");
+            FacesContext.getCurrentInstance().addMessage("editaSPForm:codigo", msg);
+            return "servicio_x_cliente_gestion.xhtml";
+        }
+    }
+    
+    public String inserta() {
+        if (ServicioPacienteGestion.insertar(this)) {
+            return "servicio_x_cliente_gestion.xhtml";
+        } else {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error",
+                    "Posiblemente identificacion duplicada");
+            FacesContext.getCurrentInstance().addMessage("editaSPForm:codigo", msg);
+            return "editasp.xhtml";
+        }
+    }
+
+    public String modifica() {
+        if (ServicioPacienteGestion.modificar(this)) {
+            return "servicio_x_cliente_gestion.xhtml";
+        } else {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error",
+                    "Posiblemente identificacion duplicada");
+            FacesContext.getCurrentInstance().addMessage("editaSPForm:codigo", msg);
+            return "editasp.xhtml";
+        }
+    }
+
+    public String elimina() {
+        if (ServicioPacienteGestion.eliminar(this)) {
+            return "servicio_x_cliente_gestion.xhtml";
+        } else {
+            FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error",
+                    "Posiblemente identificacion ya no exista");
+            FacesContext.getCurrentInstance().addMessage("editaSPForm:codigo", msg);
+            return "editasp.xhtml";
+        }
+    }
+}
